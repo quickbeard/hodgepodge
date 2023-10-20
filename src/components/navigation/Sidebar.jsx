f59de916293392f54a1010eb232ac79e67de2554
minhnh49 <minhnh49@viettel.com.vn>
2023-10-20 22:23:57 +0700

    feat: remove unused codes

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -2,16 +2,12 @@
 
 import React from "react";
 import {
-  IconButton,
   Box,
   CloseButton,
   Flex,
   Icon,
   useColorModeValue,
   Text,
-  Drawer,
-  DrawerContent,
-  useDisclosure,
 } from "@chakra-ui/react";
 import {
   FiHome,
@@ -19,7 +15,6 @@ import {
   FiCompass,
   FiStar,
   FiSettings,
-  FiMenu,
 } from "react-icons/fi";
 
 const LinkItems = [
@@ -92,58 +87,3 @@ export default function SidebarContent({ onClose, ...rest }) {
     </Box>
   );
 }
-
-// const MobileNav = ({ onOpen, ...rest }) => {
-//   return (
-//     <Flex
-//       ml={{ base: 0, md: 60 }}
-//       px={{ base: 4, md: 24 }}
-//       height="20"
-//       alignItems="center"
-//       bg={useColorModeValue("white", "gray.900")}
-//       borderBottomWidth="1px"
-//       borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-//       justifyContent="flex-start"
-//       {...rest}
-//     >
-//       <IconButton
-//         variant="outline"
-//         onClick={onOpen}
-//         aria-label="open menu"
-//         icon={<FiMenu />}
-//       />
-
-//       <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-//         Logo
-//       </Text>
-//     </Flex>
-//   );
-// };
-
-// export default function Sidebar() {
-//   const { isOpen, onOpen, onClose } = useDisclosure();
-//   return (
-//     <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.700")}>
-//       <SidebarContent
-//         onClose={() => onClose}
-//         display={{ base: "none", md: "block" }}
-//       />
-//       <Drawer
-//         isOpen={isOpen}
-//         placement="left"
-//         onClose={onClose}
-//         returnFocusOnClose={false}
-//         onOverlayClick={onClose}
-//         size="full"
-//       >
-//         <DrawerContent>
-//           <SidebarContent onClose={onClose} />
-//         </DrawerContent>
-//       </Drawer>
-//       <MobileNav display={{ base: "flex", md: "none" }} onOpen={onOpen} />
-//       <Box ml={{ base: 0, md: 60 }} p="4">
-//         Hello
-//       </Box>
-//     </Box>
-//   );
-// }
