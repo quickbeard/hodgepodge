8ffb17ae4bc17f4c9dd615395156482cd68099af
Minh N <44449792+minhn4@users.noreply.github.com>
2023-10-21 23:32:38 +0700

    feat: minor refactoring (#11)
    Co-authored-by: minhnh49 <minhnh49@viettel.com.vn>

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,3 @@
-import { PayPalButton } from "../components/PayPal";
-
 export default function Home() {
-  return (
-    <></>
-    // <div className="flex flex-col items-center justify-center h-screen">
-    //   <PayPalButton />
-    // </div>
-  );
+  return <></>;
 }
