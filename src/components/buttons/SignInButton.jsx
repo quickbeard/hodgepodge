b3ba3fa4d69f5e1a66a52eefa08407bf45f30729
minhn4 <minhn4@org.com>
2023-10-22 10:18:21 +0700

    feat: add eslintignore

--- src/components/buttons/SignInButton.jsx
+++ src/components/buttons/SignInButton.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Input } from "@chakra-ui/react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function SignInButton() {
