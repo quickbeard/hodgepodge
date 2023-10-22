3d1b71de9323908b888f992aad51f0bfda480dd0
minhn4 <minhn4@org.com>
2023-10-22 21:28:56 +0700

    feat: fix root layout

--- src/components/navigation/SidebarWithHeader.jsx
+++ src/components/navigation/SidebarWithHeader.jsx
@@ -4,14 +4,12 @@ import { Box, useColorModeValue } from "@chakra-ui/react";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 
-export default function SidebarWithHeader() {
+export default function SidebarWithHeader({ children }) {
   return (
     <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
       <Sidebar />
       <Navbar />
-      <Box ml={{ base: 0, md: 60 }} p="4">
-        Hello
-      </Box>
+      {children}
     </Box>
   );
 }
