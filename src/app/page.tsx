03adcb450fe7f418b26ba09cf3e7c29cd473f1c7
minhn4 <minhn4@org.com>
2023-10-23 17:44:43 +0700

    feat: add PayPal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Box } from "@chakra-ui/react";
-import { PayPalButton } from "@/components/PayPal";
 
 export default function Home() {
   return (
     <Box ml={{ base: 0, md: 60 }} p="4">
-      <PayPalButton></PayPalButton>
+      Hello
     </Box>
   );
 }
