f0332bdfff14b93620a95a70a832491d93fd603a
minhn4 <minhn4@org.com>
2023-10-24 23:55:10 +0700

    minor playwright config modification

--- __e2e__/paypal.spec.ts
+++ __e2e__/paypal.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("has PayPal components", async ({ page }) => {
-  await page.goto("localhost:3000/checkout");
+  await page.goto("/checkout");
   await page.locator("a").filter({ hasText: "Check Out" }).click();
 
   const paypal = page.getByRole("main", { name: "PayPal" });
