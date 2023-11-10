1c45d2d0e22d3bc3a4e1df730e7ee6f646dd5400
minhn4 <nguyenm.cuny@gmail.com>
2023-11-10 15:28:16 +0700

    minor eslint fixes

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import Auth0Provider from "next-auth/providers/auth0";
 
 import { env } from "@/env.mjs";
 import { db } from "@/server/db";
@@ -47,9 +47,10 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    Auth0Provider({
+      clientId: env.AUTH0_CLIENT_ID,
+      clientSecret: env.AUTH0_CLIENT_SECRET,
+      issuer: env.AUTH0_ISSUER_BASE_URL,
     }),
     /**
      * ...add more providers here.
