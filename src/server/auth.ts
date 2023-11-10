a774877eeff6292a43f219345b4c3818c3c81bae
minhn4 <nguyenm.cuny@gmail.com>
2023-11-10 18:00:04 +0700

    minor changes

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -52,15 +52,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.AUTH0_CLIENT_SECRET,
       issuer: env.AUTH0_ISSUER_BASE_URL,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
